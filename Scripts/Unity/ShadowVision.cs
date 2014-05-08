2dbf35e391c1373531d47c73860a062d54328613
Taylor <Tshadow05@gmail.com>
2014-05-08 12:55:17 -0700

    Initial commit

---
+++ Scripts/Unity/ShadowVision.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShadowVision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	static public string padInt(int value, int numberOfDigits){
+		return value.ToString ("D" + numberOfDigits.ToString ());
+	}
+}
